2d1ac3993d85ef6a4a305047070ec03e3b5182c7
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-07-14 11:07:44 +0530

    dbConnect and product seed  api setup

--- src/lib/models/ProductModel.ts
+++ src/lib/models/ProductModel.ts
@@ -1,3 +1,29 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    brand: { type: String, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    countInStock: { type: Number, required: true },
+    description: { type: String, required: true },
+    banner: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductModel =
+  mongoose.models.Product || mongoose.model("Product", ProductSchema);
+
+export default ProductModel;
+
 export type Product = {
   _id: string;
   name: string;
