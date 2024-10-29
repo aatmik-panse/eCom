cf3fe60ad925362e63505133aa689d655e1aa6b6
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-10-29 09:50:36 +0530

    product cards improved 2

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -5,40 +5,18 @@ import React from "react";
 
 export default function ProductItem({ product }: { product: Product }) {
   return (
-    <div className="group border-gray-100/30 flex w-full max-w-md flex-col self-center overflow-hidden rounded-lg border bg-gray-700 shadow-md">
+    <div className="group border-gray-100/30  flex w-full max-w-md flex-col self-center overflow-hidden rounded-lg border bg-gray-700 shadow-md">
       <Link
-        className="relative mx-3 mt-3 flex h-72 overflow-hidden rounded-xl"
+        className="relative mx-3 mt-3 flex h-80 overflow-hidden rounded-xl"
         href={`/product/${product.slug}`}
       >
         <Image
-          className="peer absolute top-0 right-0 h-full w-full object-cover"
+          className="w-full h-fit object-cover"
           src={product.image}
           alt="product image"
-          fill
-          style={{ height: "100%" }}
+          width={100}
+          height={100}
         />
-        <Image
-          className="peer peer-hover:right-0 absolute top-0 -right-96 h-full w-full object-cover transition-all delay-100 duration-1000 hover:right-0"
-          src={product.image}
-          alt="product image"
-          fill
-          style={{ height: "100%" }}
-        />
-        <svg
-          className="group-hover:animate-ping group-hover:opacity-30 peer-hover:opacity-0 pointer-events-none absolute inset-x-0 bottom-5 mx-auto text-3xl text-white transition-opacity"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="img"
-          width="1em"
-          height="1em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 32 32"
-        >
-          <path
-            fill="currentColor"
-            d="M2 10a4 4 0 0 1 4-4h20a4 4 0 0 1 4 4v10a4 4 0 0 1-2.328 3.635a2.996 2.996 0 0 0-.55-.756l-8-8A3 3 0 0 0 14 17v7H6a4 4 0 0 1-4-4V10Zm14 19a1 1 0 0 0 1.8.6l2.7-3.6H25a1 1 0 0 0 .707-1.707l-8-8A1 1 0 0 0 16 17v12Z"
-          />
-        </svg>
       </Link>
       <div className="mt-4 px-5 pb-5">
         <a href="#">
